bd0e52ad81cb1e8cd2ac47d695e1d85ba61aefcb
mustafaerbay <mustafaerbay365@gmail.com>
2022-02-06 19:30:30 +0300

    Dockerfile update

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,5 @@
-FROM alpine:latest AS default
-
-ARG PRODUCT_VERSION=UNSPECIFIED
-ARG BIN_NAME=cer
-ARG TARGETARCH
-
-LABEL maintainer="Mustafa Erbay <mustafaerbay365@gmail.com>"
-LABEL version=$VERSION
-LABEL "cer.version"="${VERSION}"
-
-RUN apk add --no-cache git openssh
-COPY ["dist/cer_linux_386/cer", "/bin/cer"]
-
-ENTRYPOINT ["/bin/cer"]
+FROM scratch
+# Copy our static executable.
+COPY dist/cer_linux_386/cer /go/bin/cer
+# Run the hello binary.
+ENTRYPOINT ["/go/bin/cer"]
--- manual.sh
+++ manual.sh
@@ -5,12 +5,12 @@ VERSION="$(git describe --tags --always --abbrev=0 --match='v[0-9]*.[0-9]*.[0-9]
 COMMIT_HASH="$(git rev-parse --short HEAD)"
 BUILD_TIMESTAMP=$(date '+%Y-%m-%dT%H:%M:%S')
 
-LDFLAGS=(
-  "-X 'cer/config.BuildVersion=${VERSION}'"
-  "-X 'cer/config.CommitHash=${COMMIT_HASH}'"
-  "-X 'cer/config.BuildTime=${BUILD_TIMESTAMP}'"
-)
+#LDFLAGS=(
+#  "-X 'cer/config.BuildVersion=${VERSION}'" 
+#  "-X 'cer/config.CommitHash=${COMMIT_HASH}'" 
+#  "-X 'cer/config.BuildTime=${BUILD_TIMESTAMP}'"
+#)
 
-go build -ldflags="${LDFLAGS[*]}" -o dist/cer_linux_386/cer
-
-docker build .
+#go build -ldflags="${LDFLAGS[*]}" -o dist/cer_linux_386/cer
+CGO_ENABLED=0 GOOS=linux GARCH=amd64 go build -ldflags="-X 'cer/config.BuildVersion=${VERSION}' -X 'cer/config.CommitHash=${COMMIT_HASH}' -X 'cer/config.BuildTime=${BUILD_TIMESTAMP}'" -o dist/cer_linux_386/cer
+docker build -t anatolman/cer:"${VERSION}" . 
